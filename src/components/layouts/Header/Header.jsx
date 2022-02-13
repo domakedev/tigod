e803649d1e3314875d821a3ad2071417fae34408
Cesar Guevara Cabrera <sarcansino@gmail.com>
2022-02-12 22:39:36 -0500

    Implementacion inicial de Auth0: login y registro

--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Button from "../Buttons/Button";
-import { useNavigate } from "react-router";
+import { useAuth0 } from "@auth0/auth0-react";
 import Logo from "../../../assets/Logotipo.png";
 import { Link } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import "./Header.css";
 
 const Header = () => {
-  const navigate = useNavigate();
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+  console.log(
+    "🚀 ~ file: Header.jsx ~ line 13 ~ isAuthenticated",
+    isAuthenticated
+  );
+  console.log("🚀 ~ file: Header.jsx ~ line 13 ~ user", user);
+
   return (
     <div className="header">
       <Link to="/">
@@ -15,29 +21,70 @@ const Header = () => {
       </Link>
       <div className="header_buttons">
         <Menu right isOpen={false}>
-          <Link className="menu-item " to="/login">
-            Login
-          </Link>
-          <Link className="menu-item " to="/register">
-            Register
-          </Link>
+          {isAuthenticated ? (
+            <Link
+              className="menu-item "
+              to=""
+              onClick={() => {
+                logout({ returnTo: window.location.origin });
+              }}
+            >
+              Cerrar Sesión
+            </Link>
+          ) : (
+            <>
+              <Link
+                className="menu-item "
+                to=""
+                onClick={() => {
+                  loginWithRedirect();
+                }}
+              >
+                Iniciar Sesión
+              </Link>{" "}
+              <br />
+              <Link
+                className="menu-item "
+                to=""
+                onClick={() => {
+                  loginWithRedirect({ screen_hint: "signup" });
+                }}
+              >
+                Registrarme
+              </Link>
+            </>
+          )}
         </Menu>
-        <Button
-          text="Iniciar Sesion"
-          type="secondary"
-          fun={() => {
-            navigate("/login");
-          }}
-          hidden
-        />
-        <Button
-          text="Registrarme"
-          type="principal"
-          fun={() => {
-            navigate("/register");
-          }}
-          hidden
-        />
+        <div></div>
+        {isAuthenticated ? (
+          <Button
+            text="Cerrar Sesion"
+            type="alert"
+            fun={() => {
+              logout({ returnTo: window.location.origin });
+            }}
+            hidden
+          />
+        ) : (
+          <>
+            <Button
+              text="Iniciar Sesión"
+              type="secondary"
+              fun={() => {
+                loginWithRedirect();
+              }}
+              hidden
+            />
+            <Button
+              text="Registrarme"
+              type="principal"
+              fun={() => {
+                loginWithRedirect({ screen_hint: "signup" });
+              }}
+              hidden
+            />
+          </>
+        )}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -7,17 +7,24 @@ import TestPage from "./components/pages/TestPage/TestPage";
 import Register from "./components/pages/Register/Register.jsx";
 import NotFound from "./components/pages/P404/P404.jsx";
 import MiVocacion from "./components/pages/MiVocacion/MiVocacion.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/test" element={<TestPage />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/mivocacion" element={<MiVocacion />} />
+  <Auth0Provider
+    domain="dev-z90xuxai.us.auth0.com"
+    clientId="6SSh5YK5UIcMqTowjFT1u2igaIpb8gh9"
+    redirectUri={window.location.origin}
+  >
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/mivocacion" element={<MiVocacion />} />
 
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>,
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </Auth0Provider>,
   document.getElementById("root")
 );
