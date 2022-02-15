837762386a3a42f1b10ad7ebcdeedd23ca3dbb6b
Cesar Guevara Cabrera <sarcansino@gmail.com>
2022-02-14 23:56:23 -0500

    Implementacion de Apollo Client

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import Header from "./components/layouts/Header/Header.jsx";
 import Footer from "./components/layouts/Footer/Footer.jsx";
 import CardLanding from "./components/layouts/Cards/CardLanding/CardLanding";
 
+// Apollo
+import { useQuery, gql } from "@apollo/client";
+
 // Images svg
 import Professional from "./assets/Landing/profesional.svg";
 import Thinking from "./assets/Landing/Thinking.svg";
 import Parents from "./assets/Landing/Parents.svg";
 
+const OBTENER_ESTUDIANTE = gql`
+  query obtenerEstudiante($id: ID!) {
+    obtenerEstudiante(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
 function App() {
+  const id = "620b05204f2f09b57b3ea3b7";
+  const { loading, error, data } = useQuery(OBTENER_ESTUDIANTE, {
+    variables: {
+      id,
+    },
+  });
+  console.log("🚀 ~ file: App.js ~ line 23 ~ data", data);
+  console.log("🚀 ~ file: App.js ~ line 23 ~ error", error);
+  console.log("🚀 ~ file: App.js ~ line 23 ~ loading", loading);
+
   const navigate = useNavigate();
   return (
     <div className="page-container">
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 
+// Apollo GraphQL
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
 // Pages
 import App from "./App";
 import TestPage from "./components/pages/TestPage/TestPage";
@@ -15,37 +22,44 @@ import ProfileStudentConfig from "./components/pages/Profiles/ProfileStudentConf
 import ProfileProfessional from "./components/pages/Profiles/ProfileProfessional/ProfileProfessional";
 import ProfileProfessionalConfig from "./components/pages/Profiles/ProfileProfessionalConfig/ProfileProfessionalConfig";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
 render(
   <Auth0Provider
     domain={`${process.env.REACT_APP_DOMAIN}`}
     clientId={`${process.env.REACT_APP_CLIENTID}`}
     redirectUri={window.location.origin}
   >
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/test" element={<TestPage />} />
-        <Route path="/mivocacion" element={<MiVocacion />} />
-        <Route
-          path="/miperfil/estudiante/:email"
-          element={<ProfileStudent />}
-        />
-        <Route
-          path="/miperfil/estudiante/config"
-          element={<ProfileStudentConfig />}
-        />
-        <Route
-          path="/miperfil/profesional/:email"
-          element={<ProfileProfessional />}
-        />
-        <Route
-          path="/miperfil/profesional/config"
-          element={<ProfileProfessionalConfig />}
-        />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/mivocacion" element={<MiVocacion />} />
+          <Route
+            path="/miperfil/estudiante/:email"
+            element={<ProfileStudent />}
+          />
+          <Route
+            path="/miperfil/estudiante/config"
+            element={<ProfileStudentConfig />}
+          />
+          <Route
+            path="/miperfil/profesional/:email"
+            element={<ProfileProfessional />}
+          />
+          <Route
+            path="/miperfil/profesional/config"
+            element={<ProfileProfessionalConfig />}
+          />
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   </Auth0Provider>,
   document.getElementById("root")
 );
